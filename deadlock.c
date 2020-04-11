5668215a4a379f2420702de83344588ecb428853
NITESH <53593100+nitesh1016@users.noreply.github.com>
2020-04-11 23:45:53 +0530

    final

--- deadlock.c
+++ deadlock.c
@@ -1,14 +1,6 @@
-//Ques. 11. Reena’s operating system uses an algorithm for deadlock avoidance to manage the allocation of resources say three namely A, B, and C to three processes P0, P1, and P2. Consider the following scenario as reference .user must enter the current state of system as given in this example :
-Suppose P0 has 0,0,1 instances , P1 is having 3,2,0 instances and P2 occupies 2,1,1 instances of A,B,C resource respectively.
-Also the maximum number of instances required for P0 is 8,4,3 and for p1 is 6,2,0 and finally for P2 there are 3,3,3 instances of resources A,B,C respectively. There are 3 instances of resource A, 2 instances of resource B and 2 instances of resource C available. Write a program to check whether Reena’s operating system is in a safe state or not in the following independent requests for additional resources in the
-current state:
-1.	Request1: P0 requests 0 instances of A and 0 instances of B and 2 instances of C.
-2.	Request2: P1 requests for 2 instances of A, 0 instances of B and 0 instances of C.
-All the request must be given by user as input.//
 
 
-
-//code for the above given question..
+//code for the  given question..
 
 #include<conio.h>
 #include<stdio.h>
@@ -33,19 +25,15 @@ printf("\nEnter Claim Vector:");
 for (i = 0; i < resources; i++) 
 { 
 scanf("%d", &max_res[i]);                        
-   
+}
  printf("\nEnter Allocated Resource Table:\n");
  for (i = 0; i < processes; i++) 
 {
-   
  for(j = 0; j < resources; j++) 
 {
-   
 scanf("%d", &current_s[i][j]);
-}   
 }
-  
-  
+}
 printf("\nEnter Maximum Claim Table:\n");
 for (i = 0; i < processes; i++) 
 {
@@ -54,11 +42,96 @@ for(j = 0; j < resources; j++)
 scanf("%d", &maximum_claimed[i][j]);
 }
 }
-  
-  
 printf("\nThe Claim Vector is: ");
 for (i = 0; i < resources; i++) 
 {
  printf("\t%d", max_res[i]);
 }
-printf("\nThe allocated resource table:\n");	
+printf("\nThe allocated resource table:\n");				
+ for (i = 0; i < processes; i++) 
+{
+for (j = 0; j < resources; j++) 
+{
+printf("\t%d", current_s[i][j]);
+}
+printf("\n");
+}
+printf("\nThe maximum claim table:\n");
+for (i = 0; i < processes; i++) 
+{
+for (j = 0; j < resources; j++) 
+{
+printf("\t%d", maximum_claimed[i][j]);
+}
+printf("\n");
+}
+for (i = 0; i < processes; i++) 
+{
+for (j = 0; j < resources; j++) 
+{
+allocation[j] += current_s[i][j];
+}
+}
+printf("\nAllocated resources:");
+for (i = 0; i < resources; i++) 
+{
+printf("\t%d", allocation[i]);
+}
+for (i = 0; i < resources; i++) 
+{
+available[i] = max_res[i] - allocation[i];
+}
+printf("\nAvailable resources:");
+for (i = 0; i < resources; i++) 
+{
+printf("\t%d", available[i]);
+}
+printf("\n");
+while (counter != 0) 
+{
+safe = 0;
+for (i = 0; i < processes; i++) 
+{
+if (running[i]) 
+{
+exec = 1;
+for (j = 0; j < resources; j++) 
+{
+if (maximum_claimed[i][j] - current_s[i][j] > available[j]) 
+{
+exec = 0;
+break;
+}
+}
+if (exec) 
+{
+printf("\nProcess%d is executing\n", i + 1);
+running[i] = 0;
+counter--;
+safe = 1;
+for (j = 0; j < resources; j++) 
+{
+available[j] += current_s[i][j];
+}
+break;
+}
+}
+}
+if (!safe) 
+{
+printf("\nThe processes are in unsafe state.\n");
+break;
+} 
+else 
+{
+printf("\nThe process is in safe state");
+printf("\nAvailable vector:");
+for (i = 0; i < resources; i++) 
+{
+printf("\t%d", available[i]);
+}
+printf("\n");
+}
+}
+return 0;
+}
